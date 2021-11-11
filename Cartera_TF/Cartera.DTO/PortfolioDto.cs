a7b58788459ef1012e582fb02121f43e75bede76
billbch <80244699+billbch@users.noreply.github.com>
2021-11-11 02:06:24 -0500

    Update PortfolioDto.cs

--- Cartera_TF/Cartera.DTO/PortfolioDto.cs
+++ Cartera_TF/Cartera.DTO/PortfolioDto.cs
@@ -7,6 +7,8 @@ namespace Cartera.DTO
     public class PortfolioDto
     {
         public int Id { get; set; }
+        
+        public string Name { get; set; }
 
         public double TIR { get; set; }
 
