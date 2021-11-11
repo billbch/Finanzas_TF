d5dbca7e2166467e838a356b5d9beb93a4e64c59
billbch <80244699+billbch@users.noreply.github.com>
2021-11-11 02:01:46 -0500

    Update UserDto.cs

--- Cartera_TF/Cartera.DTO/UserDto.cs
+++ Cartera_TF/Cartera.DTO/UserDto.cs
@@ -7,6 +7,8 @@ namespace Cartera.DTO
     public class UserDto
     {
         public int Id { get; set; }
+        
+        public string Name { get; set; }
 
         public string Address { get; set; }
 
