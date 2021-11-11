ef5a083331ba5b8973992c68db4a44a2204bf765
billbch <80244699+billbch@users.noreply.github.com>
2021-11-11 02:01:03 -0500

    Update User.cs

--- Cartera_TF/Cartera.Entities/User.cs
+++ Cartera_TF/Cartera.Entities/User.cs
@@ -11,10 +11,13 @@ namespace Cartera.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
+        
+        [Required]
+        public string Name { get; set; }
+        
         [Required]
         public string Address { get; set; }
-
+        
         [Required]
         [StringLength(30)]
         public string Phone { get; set; }
